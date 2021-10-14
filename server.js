e77f31a17701afc9bf0c84491e7c03f38b567f35
Glitch (glitch-hello-node) <none>
2021-10-14 08:54:48 +0000

    🍡🦁 Checkpoint ./dude.js:21410604/3463 ./server.js:21410604/5766

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
+
+
 const { Client, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILDS_MESSAGES] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+
+  
 });
 
-client.on('interactionCreate', async interaction => {
+client.on('messageCreate',  (message) => {
 
   console.log("Interacted");
 
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
-  }
+
 });
 
 
 client.login('ODk3OTE2NTk3MjMzMTQ3OTU0.YWcn_w.MwlxFkGw2cYqW4Illja5THwOUIQ');
+
