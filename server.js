0863c1dd7428645e48fa2c4500ddcc07be5ea806
Glitch (glitch-hello-node) <none>
2021-10-14 08:08:41 +0000

    🍡🍡 Checkpoint ./server.js:21410604/1760

--- server.js
+++ server.js
@@ -1,39 +1,14 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client();
 
-
-const Discord = require('discord.js');
-
-
-const client = new Discord.Client();
- console.log(client);
-const prefix = '-';
- 
-const fs = require('fs');
- 
-client.commands = new Discord.Collection();
- 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
- 
-    client.commands.set(command.name, command);
-}
- 
- 
-client.once('ready', () => {
-    console.log('Codelyon is online!');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
- 
-client.on('message', message =>{
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
- 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
- 
-    if(command === 'ping'){
-        client.commands.get('ping').execute(message, args);
-    } 
+
+client.on('message', (message) => {
+
+  console.log()
+  
 });
- 
+
 client.login('ODk3OTE2NTk3MjMzMTQ3OTU0.YWcn_w.MwlxFkGw2cYqW4Illja5THwOUIQ');
- 
- 
