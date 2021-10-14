2c03f5f265b3051884b4aa4a5958a12f466fd072
Glitch (glitch-hello-node) <none>
2021-10-14 08:43:46 +0000

    🚙🍜 Checkpoint ./server.js:21410604/793 ./dude.js:21410604/1518

--- dude.js
+++ dude.js
@@ -1,24 +1,33 @@
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json');
+const fs = require('fs');
 
-const commands = [{
-  name: 'ping',
-  description: 'Replies with Pong!'
-}]; 
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-const rest = new REST({ version: '9' }).setToken('token');
+// Place your client and guild ids here
+const clientId = '897916597233147954';
+const guildId = '734404408606982215';
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
 
 (async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
+	try {
+		console.log('Started refreshing application (/) commands.');
 
-    await rest.put(
-      Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
-      { body: commands },
-    );
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
 
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
-  }
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
 })();
--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const Discord =require("discord.js");
-
-const intents = new Discord.Intents(8589934591);
-const client = new Discord.Client({intents});
-
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (message) => {
+client.on('interactionCreate', async interaction => {
 
-  console.log(message)
-  
+  console.log("Interacted");
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
 });
+
 
 client.login('ODk3OTE2NTk3MjMzMTQ3OTU0.YWcn_w.MwlxFkGw2cYqW4Illja5THwOUIQ');
