bc6d58e53f3dd5fd5998ef807c91c89e3ea561de
Glitch (glitch-hello-node) <none>
2021-10-14 15:25:04 +0000

    🐋📅 Checkpoint ./commands/command.js:21410604/452

--- commands/command.js
+++ commands/command.js
@@ -64,7 +64,7 @@ const resource = createAudioResource(stream);
 player.play(resource);       
 const subscription = connection.subscribe(player);
           
-
+message.channel.send("**"+song.title+"**"+" сейчас играет");
           
             //If the server queue does not exist (which doesn't for the first video queued) then create a constructor to be added to our global queue.
   
